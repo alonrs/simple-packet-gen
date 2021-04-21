07dc536a5e1e94087787b7f55d2a23f132cd958a
alon@acsl-serv02 <alonrs@gmail.com>
2021-04-21 05:15:28 +0000

    Fixed to build.sh

--- build.sh
+++ build.sh
@@ -4,19 +4,27 @@ dpdk_dir=$(readlink -f $(dirname $0))/dpdk
 build_dir=$dpdk_dir/build
 install_dir=$build_dir/install
 
-if [[ -f $build_dir ]]; then
+# Fetch DPDK repository
+if [[ ! -d $dpdk_dir || -z $(ls $dpdk_dir/*) ]]; then
+    echo "Fetching DPDK repository from git..."
+    git submodule update --init
+fi
+cd $dpdk_dir
+
+# Create the DPDK build dir
+if [[ -d $build_dir ]]; then
     read -p "DPDK build dir exists. Do you want to delete it? [Y|n]" ans
     if [[ -z $ans || $ans =~ [Yy] ]]; then
         rm $build_dir
     fi
 fi
 
-git submodule update --init
-cd $dpdk_dir
-
+echo "Configuring DPDK..."
 meson -Dprefix=$install_dir build
-
 cd build
+
+# Make DPDK, install locally
+echo "Making DPDK..."
 ninja
 ninja install
 
