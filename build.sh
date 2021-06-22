507e108ba3bdfdf7f5ab230bd31ea2c8c34c59a7
alon@os02 <alonrs@gmail.com>
2021-06-22 10:17:01 +0300

    Changes to libcommon version

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ sudo whoami
 if [[ ! -d libcommon/bin ]]; then
     echo "Building libcommon..."
     git submodule update --init
+    git -C libcommon checkout 10be038bf9c5a553f9d0ed241b5a3d8dd19306d9
     make -C libcommon
 fi
 
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ bdir=$(readlink -f $(dirname $0))
 ldir=$(readlink -f $(dirname $(find -name "librte_ethdev.so.20.0" | head -1)))
 
 # Start DPDK as sudo with a custom library path (GDB)
-if [[ $1 -eq gdb ]]; then
+if [[ $# -gt 0 && $1 -eq gdb ]]; then
     sudo LD_LIBRARY_PATH=$ldir gdb --args $bdir/bin/client.exe "${@:2}"
     exit 0
 fi
