d8044276b360db38a0bc17a4ae0c05d2afb5af05
Alon Rashelbach <alonrs@gmail.com>
2024-03-14 15:27:43 +0200

    Minor update to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 bdir=$(readlink -f $(dirname $0))
-ldir=$(readlink -f $(dirname $(find -name "librte_ethdev.so.20.0" | head -1)))
+echo "Searching for DPDK library in '$bdir'..."
+
+ldir=$(readlink -f $(dirname $(find $bdir -name "librte_ethdev.so.20.0" | head -1)))
+if [ -z $ldir ]; then
+        echo "ERROR: did not find DPDK library in '$bdir'"
+        exit 1
+fi
+echo "Found DPDK library in: '$ldir'"
 
 # Start DPDK as sudo with a custom library path (GDB)
 if [[ $# -gt 0 && $1 -eq gdb ]]; then
